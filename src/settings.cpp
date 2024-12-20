3ee664e93f871ac80048a618953d129e3f88b267
Leukhos <leukhos@fastmail.com>
2024-12-20 10:47:51 +0000

    feat: Allow overriding gamelist filename via config.ini and cli

--- src/settings.cpp
+++ src/settings.cpp
@@ -194,6 +194,10 @@ void RuntimeCfg::applyConfigIni(CfgType type, QSettings *settings,
                 config->extensions = v;
                 continue;
             }
+            if (k == "gameListFilename") {
+                config->gameListFilename = v;
+                continue;
+            }
             if (k == "gameListFolder") {
                 config->gameListFolder = (type == CfgType::MAIN ||
                                           type == CfgType::FRONTEND /* #68 */)
@@ -622,6 +626,9 @@ void RuntimeCfg::applyCli(bool &inputFolderSet, bool &gameListFolderSet,
             Cli::cacheReportMissingUsage();
             exit(0);
         }
+    }
+    if (parser->isSet("gamelistfilename")) {
+        config->gameListFilename = parser->value("gamelistfilename");
     }
     if (parser->isSet("startat")) {
         config->startAt = parser->value("startat");
